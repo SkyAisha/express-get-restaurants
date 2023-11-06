0d3df3fb75da6e666f07f46dfabe9cc19e0e327b
SkyAisha <aisha.adia@sky.uk>
2023-11-06 10:45:17 +0000

    express restaurant part 1 completed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
-const Restaurant = require("../models/index")
+const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
-//TODO: Create your GET Request Route Below: 
+//TODO: Create your GET Request Route Below:
 
-
-
+app.get("/restaurants", async (request, response) => {
+  // WRITE YOUR CODE HERE
+  const restaurants = await Restaurant.findAll();
+  response.json(restaurants);
+});
 
 module.exports = app;
