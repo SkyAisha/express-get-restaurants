f37a1cb245eb45991a76a447f6217fd9f0f3165f
SkyAisha <aisha.adia@sky.uk>
2023-11-09 11:12:50 +0000

    express-validator

--- src/app.js
+++ src/app.js
@@ -3,37 +3,39 @@ const app = express();
 const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
+const restaurantRouter = require("../routes/restaurants");
+app.use("/restaurants", restaurantRouter);
 //TODO: Create your GET Request Route Below:
 
-app.get("/restaurants", async (request, response) => {
-  // WRITE YOUR CODE HERE
-  const restaurants = await Restaurant.findAll();
-  response.json(restaurants);
-});
+// app.get("/restaurants", async (request, response) => {
+//   // WRITE YOUR CODE HERE
+//   const restaurants = await Restaurant.findAll();
+//   response.json(restaurants);
+// });
 
-app.get("/restaurants/:id", async (req, res) => {
-  const id = req.params.id;
-  const foundRestaurant = await Restaurant.findByPk(id);
-  res.json(foundRestaurant);
-});
+// app.get("/restaurants/:id", async (req, res) => {
+//   const id = req.params.id;
+//   const foundRestaurant = await Restaurant.findByPk(id);
+//   res.json(foundRestaurant);
+// });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-app.post("/restaurants", async (req, res) => {
-  const newRestaurant = await Restaurant.create(req.body);
-  res.send("New Restaurant");
-});
+// app.post("/restaurants", async (req, res) => {
+//   const newRestaurant = await Restaurant.create(req.body);
+//   res.send("New Restaurant");
+// });
 
-app.put("/restaurants/:id", async (req, res) => {
-  const updatedRestaurant = await Restaurant.update(req.body, {
-    where: { id: req.params.id },
-  });
-  res.send("Restaurant updated");
-});
+// app.put("/restaurants/:id", async (req, res) => {
+//   const updatedRestaurant = await Restaurant.update(req.body, {
+//     where: { id: req.params.id },
+// });
+// res.send("Restaurant updated");
+// });
 
-app.delete("/restaurants/:id", async (req, res) => {
-  await Restaurant.destroy({ where: { id: req.params.id } });
-  res.send("Restaurant deleted");
-});
+// app.delete("/restaurants/:id", async (req, res) => {
+// await Restaurant.destroy({ where: { id: req.params.id } });
+// res.send("Restaurant deleted");
+// });
 module.exports = app;
